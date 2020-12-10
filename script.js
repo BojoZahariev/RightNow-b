8186bf9847e91566e29fd451ea9280cb0dc69b5a
Bojo <bozhidar_zahariev@hotmail.com>
2020-12-10 11:00:47 +0000

    padding

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ $(document).ready(function() {
     $('#time').text(time);
 
     $('#trees').text(time * 475);
+    $('#treesText').text('Trees cut');
     $('#plastic').text(time * 93);
+    $('#plasticText').text('Plastic pieces in the ocean');
     $('#co2').text(time * 1165732);
+    $('#co2Text').text('Kilograms of CO2 emitted in the air');
     $('#ice').text(time * 8000000);
+    $('#iceText').text('Kilograms of Ice melting in Antarctica');
     $('#methane').text(time * 18000);
+    $('#methaneText').text('Kilograms of methane emitted in the air');
 
     time += 1;
   }, 1000);
