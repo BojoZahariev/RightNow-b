93b227331521ec40a0d33a3dc107cbee91850ee9
Bojo <bozhidar_zahariev@hotmail.com>
2020-12-10 10:27:55 +0000

    numbers

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ jQuery(function($) {
   setInterval(function() {
     $('#time').text(time);
 
+    $('#trees').text(time * 475);
+    $('#plastic').text(time * 93);
+    $('#co2').text(time * 1165732);
+    $('#ice').text(time * 8000000);
+    $('#methane').text(time * 18000);
+
     time += 1;
   }, 1000);
 });
